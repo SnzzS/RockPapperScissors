b82aa28c966b0dceddc1e19bc4a9688d518a551c
Snzz <dnsafr@gmail.com>
2022-01-23 22:18:29 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+import random
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+def one_d():
+    #random.randint(1, 10)
+    print(f'{random.randint(-10, 10)} in the Building')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    one_d()
